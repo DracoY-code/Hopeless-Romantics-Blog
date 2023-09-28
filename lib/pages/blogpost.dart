ed12bdf9a50a96a42320f9e5b7c89aa2c55d5329
Yashvardhan Singh <draco882003@gmail.com>
2023-09-29 00:10:51 +0530

    Add Project files

---
+++ lib/pages/blogpost.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BlogPostPage extends StatelessWidget {
+  const BlogPostPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
